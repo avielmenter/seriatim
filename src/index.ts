d36e9c033420479f21fba3af4910420573d7e777
Aviel Menter <aviel.menter@gmail.com>
2018-07-14 03:24:58 -0700

    hiding facebook button in production

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,13 @@ var app = Elm.LoginWidget.Main.embed(mountNode, {
 	seriatim_client_url: SERIATIM_CLIENT_URL,
 	seriatim_server_url: SERIATIM_SERVER_URL
 });
+
+if (!SERIATIM_CLIENT_URL.includes("localhost")) {	// because we don't have https, hide the (nonfunctional) facebook login button in production
+	let style = document.createElement("style") as HTMLStyleElement;
+	style.appendChild(document.createTextNode(""));
+
+	if (style.sheet)
+		(style.sheet as CSSStyleSheet).insertRule("#loginFacebook { visibility: hidden }");
+
+	document.head.appendChild(style);
+}

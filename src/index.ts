50120742ce9a5a905462d3f349c069fb05ba4351
avielmenter <aviel.menter@gmail.com>
2018-07-14 04:45:57 -0700

    site is now on https

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,3 @@ var app = Elm.LoginWidget.Main.embed(mountNode, {
 	seriatim_client_url: SERIATIM_CLIENT_URL,
 	seriatim_server_url: SERIATIM_SERVER_URL
 });
-
-if (!SERIATIM_CLIENT_URL.includes("localhost")) {	// because we don't have https, hide the (nonfunctional) facebook login button in production
-	let style = document.createElement("style") as HTMLStyleElement;
-	style.appendChild(document.createTextNode(""));
-
-	document.head.appendChild(style);
-
-	if (style.sheet)
-		(style.sheet as CSSStyleSheet).insertRule("#loginFacebook { display: none !important }");
-
-}

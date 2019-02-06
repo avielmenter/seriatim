1edcb258ce57569911b6718b27dd18b60da6f151
avielmenter <aviel.menter@gmail.com>
2019-02-06 15:05:47 -0500

    simplified itemeditor lifecycle with hooks

--- src/editor/components/item/itemEditor.tsx
+++ src/editor/components/item/itemEditor.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 
-import { DispatchProps, mapDispatchToProps, ApplicationState } from '../../store';
+import { DispatchProps, mapDispatchToProps, ApplicationState, handleClick } from '../../store';
 
 import { ListItem, CursorPosition } from '../../store/data/item';
 
@@ -13,114 +13,76 @@ type AttrProps = {
 
 type ComponentProps = StateProps & AttrProps & DispatchProps;
 
-class ItemEditor extends React.Component<ComponentProps> {
-	editArea: React.RefObject<HTMLTextAreaElement>;
+function getCursorPosition(
+	event: React.KeyboardEvent<HTMLTextAreaElement> | React.MouseEvent<HTMLTextAreaElement> | React.ChangeEvent<HTMLTextAreaElement>,
+	editArea: React.RefObject<HTMLTextAreaElement>
+): CursorPosition | undefined {
+	if (!editArea.current)
+		return undefined;
 
-	constructor(props: ComponentProps) {
-		super(props);
-		this.editArea = React.createRef<HTMLTextAreaElement>();
-	}
+	const start = editArea.current.selectionStart;
+	const length = editArea.current.selectionEnd - start;
 
-	getTextAreaId() {
-		return '__edit__' + this.props.node.item.itemID;
-	}
+	return {
+		start: start + length,
+		length: 0
+	};
+}
 
-	setSelectionRange(position?: CursorPosition): void {
-		const item = this.props.node.item;
+function resizeTextArea(editArea: React.RefObject<HTMLTextAreaElement>) {
+	const textArea = editArea.current;
+	if (!textArea)
+		return;
 
-		if (this.editArea.current) {
-			const selectionEnd = this.editArea.current.value.length;
-			const selectionStart = this.props.node.itemType == "Title" && item.text == "Untitled Document" ? 0 : selectionEnd;
+	textArea.style.height = 'auto';
+	textArea.style.height = (25 + textArea.scrollHeight) + 'px';
+}
 
-			const start = !position ? selectionStart : position.start;
-			const length = !position ? selectionEnd - selectionStart : position.length;
+function shouldCursorUpdate(event: React.KeyboardEvent<HTMLTextAreaElement>): boolean {
+	return (event.key < 'a' || event.key > 'z' || event.ctrlKey)
+		&& event.key.toLowerCase() != 'backspace'
+		&& event.key.toLowerCase() != 'delete'
+		&& event.key != ' ';
+}
 
-			this.editArea.current.setSelectionRange(start, start + length);
-			this.props.actions.item.updateCursor(item, {
-				start,
-				length
-			});
+const ItemEditor: React.SFC<ComponentProps> = (props) => {
+	const { item } = props.node;
+	const { actions } = props;
+
+	const editArea: React.RefObject<HTMLTextAreaElement> = React.useRef<HTMLTextAreaElement>(null);
+
+	React.useEffect(() => {
+		if (props.node.focused && editArea.current)
+			editArea.current.focus();
+
+		resizeTextArea(editArea);
+
+		return () => {
+			actions.item.updateCursor(item, undefined);
 		}
-	}
+	}, [props.node.focused]);
 
-	getCursorPosition(event: React.KeyboardEvent<HTMLTextAreaElement> | React.MouseEvent<HTMLTextAreaElement> | React.ChangeEvent<HTMLTextAreaElement>): CursorPosition | undefined {
-		if (!this.editArea.current)
-			return undefined;
-
-		const item = this.props.node.item;
-
-		const start = this.editArea.current.selectionStart;
-		const length = this.editArea.current.selectionEnd - start;
-
-		return {
-			start: start + length,
-			length: 0
-		};
-	}
-
-	focusOnTextArea() {
-		if (this.props.node.focused && this.editArea.current)
-			this.editArea.current.focus();
-	}
-
-	resizeTextArea() {
-		const textArea = this.editArea.current;
-		if (!textArea)
-			return;
-
-		textArea.style.height = 'auto';
-		textArea.style.height = (25 + textArea.scrollHeight) + 'px';
-	}
-
-	componentDidMount() {
-		this.focusOnTextArea();
-
-		this.resizeTextArea();
-		this.setSelectionRange(this.props.node.item.view.cursorPosition);
-	}
-
-	render() {
-		const { item } = this.props.node;
-		const { actions } = this.props;
-
-		return (
-			<textarea id={this.getTextAreaId()} className="editArea"
-				onChange={(event) => {
-					this.resizeTextArea();
-					actions.item.updateItemText(item, event.target.value);
-				}}
-				onKeyUp={(event) => {
-					if ((event.key < 'a' || event.key > 'z' || event.ctrlKey)
-						&& event.key.toLowerCase() != 'backspace'
-						&& event.key.toLowerCase() != 'delete'
-						&& event.key != ' ') {	// only update cursor if updateItemText didn't 
-						this.props.actions.item.updateCursor(item, this.getCursorPosition(event));
-					} else if (!event.ctrlKey && item.view.cursorPosition && item.view.cursorPosition.length > 0) { // also update if we need to collapse the selection
-						this.props.actions.item.updateCursor(item, {
-							start: item.view.cursorPosition.start + item.view.cursorPosition.length,
-							length: 0
-						});
-					}
-				}}
-				onClick={(event) => this.props.actions.item.updateCursor(item, this.getCursorPosition(event))}
-				value={item.text}
-				ref={this.editArea}
-			></textarea>
-		);
-	}
-
-	componentDidUpdate() {
-		this.focusOnTextArea();
-
-		const cursorPosition = this.props.node.item.view.cursorPosition;
-
-		if (this.editArea.current && this.editArea.current.selectionStart != (!cursorPosition ? 0 : cursorPosition.start))
-			this.setSelectionRange(this.props.node.item.view.cursorPosition);
-	}
-
-	componentWillUnmount() {
-		this.props.actions.item.updateCursor(this.props.node.item, undefined);
-	}
+	return (
+		<textarea id={'__edit__' + item.itemID} className="editArea"
+			onChange={(event) => {
+				resizeTextArea(editArea);
+				actions.item.updateItemText(item, event.target.value);
+			}}
+			onKeyUp={(event) => {
+				if (shouldCursorUpdate(event)) {	// only update cursor if updateItemText didn't 
+					props.actions.item.updateCursor(item, getCursorPosition(event, editArea));
+				} else if (!event.ctrlKey && item.view.cursorPosition && item.view.cursorPosition.length > 0) { // also update if we need to collapse the selection
+					props.actions.item.updateCursor(item, {
+						start: item.view.cursorPosition.start + item.view.cursorPosition.length,
+						length: 0
+					});
+				}
+			}}
+			onClick={handleClick((event) => props.actions.item.updateCursor(item, getCursorPosition(event, editArea)))}
+			value={item.text}
+			ref={editArea}
+		></textarea>
+	);
 }
 
 const mapStateToProps = (state: ApplicationState | {}) => ({});
